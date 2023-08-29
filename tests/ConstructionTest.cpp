9198d5f77a3dc43c37e4007bb70e4b33862bd9a1
Master92 <nils.friedchen@googlemail.com>
2023-08-29 09:14:25 +0200

    Rename ConstructionTest.cpp

--- tests/ConstructionTest.cpp
+++ tests/ConstructionTest.cpp
@@ -1,9 +1,9 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
-#include <cppIni/cppIni.h>
 #include <doctest/doctest.h>
+#include <cppIni/cppIni.h>
 
 TEST_CASE("Construction test")
 {
-    CHECK(one() == 1);
+    CHECK_EQ(one(), 1);
 }
