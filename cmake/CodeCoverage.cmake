af0c171ba621ecb84e3f9a78aada03b1486c582a
Master92 <nils.friedchen@googlemail.com>
2023-12-01 08:03:33 +0100

    Add code coverage CMake configuration

---
+++ cmake/CodeCoverage.cmake
@@ -0,0 +1,13 @@
+cmake_minimum_required(VERSION 3.24)
+
+# Code coverage configuration
+add_library(coverage_config INTERFACE)
+if(CODE_COVERAGE AND CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+    message("Enabling code coverage")
+    target_compile_options(coverage_config INTERFACE
+            -O0             # no optimization
+            -g              # generate debug info
+            --coverage      # sets all required flags
+    )
+    target_link_options(coverage_config INTERFACE --coverage)
+endif()
