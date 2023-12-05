3f25dfe7bc0d8129602d17e23fb4fd4427e7ed42
Master92 <nils.friedchen@googlemail.com>
2023-12-05 13:22:48 +0100

    Replace conanfile.txt with python script

---
+++ conanfile.py
@@ -0,0 +1,89 @@
+#  cppIni - A C++20 library for reading and writing INI files
+#  Copyright (C) 2023 Nils Hofmann <nils.friedchen@googlemail.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+
+
+class cppiniRecipe(ConanFile):
+    name = "cppini"
+    version = "0.1.0"
+    package_type = "library"
+
+    # Optional metadata
+    license = "GPL-3.0-or-later"
+    author = "Nils Hofmann <nils.friedchen@gooelamil.com>"
+    url = "https://github.com/Master92/cppIni"
+    description = "A C++20 library for reading and writing INI files"
+    topics = ("c++20", "configuration", "ini")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "testing": [True, False]
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "testing": True
+    }
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "src/*", "include/*", "cmake/*", "tests/*"
+
+    def build_requirements(self):
+        self.build_requires("cmake/[>=3.24]")
+        if self.options.testing:
+            self.test_requires("doctest/[>=2.4]")
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.rm_safe("fPIC")
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, "20")
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_SHARED_LIBS"] = self.options.shared
+        tc.variables["BUILD_TESTING"] = self.options.testing
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        cmake.test()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["cppini"]
