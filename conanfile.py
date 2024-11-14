abec693203559eae52d9d4b54af2ae375a5e7c19
Master92 <nils.friedchen@googlemail.com>
2024-11-14 16:00:58 +0100

    Bump version to 0.2.1

--- conanfile.py
+++ conanfile.py
@@ -26,6 +26,19 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
@@ -34,7 +47,7 @@ from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
 class cppiniRecipe(ConanFile):
     name = "cppini"
-    version = "0.2.0"
+    version = "0.2.1"
     package_type = "library"
 
     # Optional metadata
